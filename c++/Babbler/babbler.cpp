e6d8556e3510e4767e36f9e4106bfd250581a1cf
Cody Brown <codybrowncit@gmail.com>
2015-02-24 23:04:12 -0700

    put back

---
+++ c++/Babbler/babbler.cpp
@@ -0,0 +1,9 @@
+//
+//  babbler.cpp
+//  Babbler
+//
+//  Created by Cody Brown on 12/8/14.
+//
+//
+
+#include "babbler.h"
---
+++ c++/Babbler/babbler.h
@@ -0,0 +1,14 @@
+//
+//  babbler.h
+//  Babbler
+//
+//  Created by Cody Brown on 12/8/14.
+//
+//
+
+#ifndef __Babbler__babbler__
+#define __Babbler__babbler__
+
+#include <stdio.h>
+
+#endif /* defined(__Babbler__babbler__) */
